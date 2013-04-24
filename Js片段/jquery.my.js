592e2f119b1ab5773dfe929b638faae8384dca3f
lc_liuyang <lc_liuyang@163.com>
2013-04-24 16:47:51 +0800

    jquery实用插件

---
+++ Js片段/jquery.my.js
@@ -0,0 +1,44 @@
+$.extend({
+    format: function (source, params) {
+        if (arguments.length == 1)
+            return function () {
+                var args = $.makeArray(arguments);
+                args.unshift(source);
+                return $.format.apply(this, args);
+            };
+        if (arguments.length > 2 && params.constructor != Array) {
+            params = $.makeArray(arguments).slice(1);
+        }
+        if (params.constructor != Array) {
+            params = [params];
+        }
+        $.each(params, function (i, n) {
+            source = source.replace(new RegExp("\\{" + i + "\\}", "g"), n);
+        });
+        return source;
+    },
+    request: function (paras) {
+        var url = location.href;
+        var paraString = url.substring(url.indexOf("?") + 1, url.length).split("&");
+        var paraObj = {}
+        for (i = 0; j = paraString[i]; i++) {
+            paraObj[j.substring(0, j.indexOf("=")).toLowerCase()] = j.substring(j.indexOf("=") + 1, j.length);
+        }
+        var returnValue = paraObj[paras.toLowerCase()];
+        if (typeof (returnValue) == "undefined") {
+            return "";
+        } else {
+            return  decodeURIComponent(returnValue);
+        }
+    }
+});
+//使用方法
+$(function () {
+    var tem = "{0} is {1}";
+    document.write($.format(tem, "this", "format"));
+    //结果this is format
+
+    //当前URL:/index.html?para=111
+    document.write($.request("para"));
+    //结果:111
+})
