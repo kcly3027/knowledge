43d72701c27b138e4fbc5ae01a5a1684ea8850f0
lc_liuyang <lc_liuyang@163.com>
2013-11-06 16:27:30 +0800

    优化Chrome插件

--- MyChromePlugin/js/down1.js
+++ MyChromePlugin/js/down1.js
@@ -1,4 +1,20 @@
 (function(window){
+	var FileList = { Count:0 };
+	var scripts = document.getElementsByTagName("script");
+	for (var i = scripts.length - 1; i >= 0; i--) {
+		var rep = /var filelist(\d*)=([^;]*);([^wW]*)ViewSingleCommon\('([^']*)', '([^']*)', '([^']*)', '([^']*)'\);/;
+		if(rep.test(scripts[i].innerHTML)){
+			var regexpArray = rep.exec(scripts[i].innerHTML);
+			eval("regexpArray[2] = " + regexpArray[2]);
+			FileList[regexpArray[4]] = regexpArray;
+			FileList.Count++;
+
+			var aList = scripts[i].parentElement.getElementsByTagName("a");
+			for (var ii = regexpArray[2].length - 1; ii >= 0; ii--) {
+				aList[ii].href = regexpArray[2][ii].url;
+			};
+		}
+	};
 	function GetDownUrl(down_url){
 		var URL = {
 		    getParameter: function(parameter, url){
@@ -22,36 +38,29 @@
 		    dl_url = "http://bt.box.n0808.com/"+cid.substr(0,2)+"/"+cid.substr(cid.length-2,2)+"/"+cid+".torrent";
 		    return dl_url;
 		}
-		if (batch == '1')
-		{
-			/*
-			chrome.extension.sendRequest({action:"getParam"}, function(response) {
-			  console.log(response.farewell);
-			});
-		    //var filelist = window.filelist.concat();
-		    //return filelist;
-		    */
-		    return "";
-		}
 		else if (batch != '')
 		{
-		    //var listArr = eval("window.filelist"+batch);
-		    //var filelist = listArr.concat();
-		    //return filelist;
 		    return "";
 		}
 		return dl_url;
 	}
+	
 	var aList = document.getElementsByTagName("a");
 	for(var i = 0;i<aList.length;i++){ 
 		if(aList[i].href.indexOf("xiazai.html")>=0)
 		{
+			var href = GetDownUrl(aList[i].href);
+			if(href == "") aList[i].style.display="none";
+			else
+				aList[i].href = href;
+			/*
 			aList[i].onclick = function(event){
 				var href = GetDownUrl(this.href)
 				if(href == "") return true;
 				chrome.extension.sendRequest({action:"tabs_create",dl_url: href}, function(response) {});
 				return false;			
 			}
+			*/
 		}
 	}
 })(window);
